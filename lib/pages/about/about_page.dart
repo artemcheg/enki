91573ec8e1a0beef3010dd1eae58393251508a13
artemcheg <artemcheg88@bk.ru>
2022-03-24 20:11:24 +0300

    исправления

--- lib/pages/about/about_page.dart
+++ lib/pages/about/about_page.dart
@@ -18,7 +18,7 @@ class AboutMain extends StatelessWidget {
       body: LayoutBuilder(
         builder: (BuildContext context, BoxConstraints constraints) {
           if (SizeWidget.isSmallScreen(context)) {
-            return Container();
+            return const AboutSmall();
           } else {
             return const About();
           }
@@ -34,119 +34,133 @@ class About extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var width = MediaQuery.of(context).size.width;
-    return Scaffold(
-      drawer: const MyDrawer(),
-      body: SingleChildScrollView(
-        child: Column(
-          // crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Container(
-              decoration: BoxDecoration(boxShadow: [
-                BoxShadow(
-                  color: Colors.black.withOpacity(0.5),
-                  spreadRadius: 5,
-                  blurRadius: 10,
-                  offset: const Offset(0, 3), // changes position of shadow
-                ),
-              ]),
-              child: SizeWidget.isSmallScreen(context)
-                  ? const SmallAppBar(
-                      shadow: null,
-                      colorDrawer: Colors.white,
-                    )
-                  : PrefSizeAppBar(
-                      dividerColor: Colors.white54,
-                    ),
+    return SingleChildScrollView(
+      child: Column(
+        // crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            decoration: BoxDecoration(boxShadow: [
+              BoxShadow(
+                color: Colors.black.withOpacity(0.5),
+                spreadRadius: 5,
+                blurRadius: 10,
+                offset: const Offset(0, 3), // changes position of shadow
+              ),
+            ]),
+            child: PrefSizeAppBar(
+              dividerColor: Colors.white54,
             ),
-            Container(
-              padding: const EdgeInsets.only(
-                  top: 20, bottom: 20, left: 20, right: 20),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const SizedBox(
-                    height: 30,
-                  ),
-                   Align(
-                      alignment: Alignment.center,
-                      child: _TextItems(
-                        text: StringRes.aboutFullZero, fontSize: sizeParam(width, 0.015, 20), fontWeight: FontWeight.w800,
-                      )),
-                  const SizedBox(
-                    height: 30,
-                  ),
-                  GridView.count(
-                    physics: const NeverScrollableScrollPhysics(),
-                    shrinkWrap: true,
-                    // crossAxisSpacing:50,
-                    mainAxisSpacing: SizeWidget.isSmallScreen(context) ? 50 : 0,
-                    crossAxisCount: SizeWidget.isSmallScreen(context) ? 1 : 2,
-                    childAspectRatio: 1.5,
-                    children: [
-                      Row(
-                        children: [
-                          Image.asset("assets/about/0.jpg"),
-                          Image.asset("assets/about/00.jpg"),
-                        ],
-                      ),
-                      const _MyText(
-                        textMain: StringRes.aboutFullOne,
-                        textTitle: 'Наши приоритеты',
-                      ),
-                      const _MyText(
-                        textMain: StringRes.aboutFullTwo,
-                        textTitle: 'Уникальность компании «ENKI»',
-                      ),
-                      Row(
-                        children: [
-                          Image.asset(
-                            'assets/about/1.jpg',
-                          ),
-                          Image.asset(
-                            'assets/about/2.jpg',
-                          ),
-                        ],
-                      ),
-                      Image.asset(
-                        'assets/about/3.jpeg',
-                      ),
-                      const _MyText(
-                        textMain: StringRes.aboutFullThree,
-                        textTitle: 'Индивидуальный подход к каждому',
-                      ),
-                      const _MyText(
-                        textMain: StringRes.aboutFullFour,
-                        textTitle: 'Пунктуальность',
-                      ),
-                      Image.asset(
-                        'assets/about/4.jpg',
-                      ),
-                    ],
-                  ),
-                  const SizedBox(height: 30),
-                   Align(
+          ),
+          Container(
+            padding:
+                const EdgeInsets.only(top: 20, bottom: 20, left: 20, right: 20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const SizedBox(
+                  height: 30,
+                ),
+                Align(
                     alignment: Alignment.center,
                     child: _TextItems(
-                      text: StringRes.aboutFullFive, fontSize: sizeParam(width, 0.015, 20), fontWeight: FontWeight.w800,
+                      text: StringRes.aboutFullZero,
+                      fontSize: sizeParam(width, 0.015, 20),
+                      fontWeight: FontWeight.w800,
+                    )),
+                const SizedBox(
+                  height: 30,
+                ),
+                GridView.count(
+                  physics: const NeverScrollableScrollPhysics(),
+                  shrinkWrap: true,
+                  mainAxisSpacing: 0,
+                  crossAxisCount: 2,
+                  childAspectRatio: 1.5,
+                  children: [
+                    Row(
+                      children: [
+                        Image.asset("assets/about/0.jpg"),
+                        Image.asset("assets/about/00.jpg"),
+                      ],
                     ),
+                    const _MyText(
+                      textMain: StringRes.aboutFullOne,
+                      textTitle: 'Наши приоритеты',
+                    ),
+                    const _MyText(
+                      textMain: StringRes.aboutFullTwo,
+                      textTitle: 'Уникальность компании «ENKI»',
+                    ),
+                    Row(
+                      children: [
+                        Image.asset(
+                          'assets/about/1.jpg',
+                        ),
+                        Image.asset(
+                          'assets/about/2.jpg',
+                        ),
+                      ],
+                    ),
+                    Image.asset(
+                      'assets/about/3.jpeg',
+                    ),
+                    const _MyText(
+                      textMain: StringRes.aboutFullThree,
+                      textTitle: 'Индивидуальный подход к каждому',
+                    ),
+                    const _MyText(
+                      textMain: StringRes.aboutFullFour,
+                      textTitle: 'Пунктуальность',
+                    ),
+                    Image.asset(
+                      'assets/about/4.jpg',
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 30),
+                Align(
+                  alignment: Alignment.center,
+                  child: _TextItems(
+                    text: StringRes.aboutFullFive,
+                    fontSize: sizeParam(width, 0.015, 20),
+                    fontWeight: FontWeight.w800,
                   ),
-                  const SizedBox(height: 30),
-                  _TextItems(text: '1. ${StringRes.aboutFullSix}', fontSize: sizeParam(width, 0.01,14), fontWeight: FontWeight.w600,),
-                  const SizedBox(height: 15),
-                  _TextItems(text: '2. ${StringRes.aboutFullSeven}', fontSize: sizeParam(width, 0.01,14), fontWeight: FontWeight.w600,),
-                  const SizedBox(height: 15),
-                  _TextItems(text: '3. ${StringRes.aboutFullEight}', fontSize: sizeParam(width, 0.01,14), fontWeight: FontWeight.w600,),
-                  const SizedBox(height: 15),
-                  _TextItems(text: '4. ${StringRes.aboutFullNine}', fontSize: sizeParam(width, 0.01,14), fontWeight: FontWeight.w600,),
-
-                  const SizedBox(height: 30),
-                   _TextItems(text: StringRes.aboutFullTen, fontSize: sizeParam(width, 0.015, 20), fontWeight: FontWeight.w800,),
-                ],
-              ),
+                ),
+                const SizedBox(height: 30),
+                _TextItems(
+                  text: '1. ${StringRes.aboutFullSix}',
+                  fontSize: sizeParam(width, 0.01, 14),
+                  fontWeight: FontWeight.w600,
+                ),
+                const SizedBox(height: 15),
+                _TextItems(
+                  text: '2. ${StringRes.aboutFullSeven}',
+                  fontSize: sizeParam(width, 0.01, 14),
+                  fontWeight: FontWeight.w600,
+                ),
+                const SizedBox(height: 15),
+                _TextItems(
+                  text: '3. ${StringRes.aboutFullEight}',
+                  fontSize: sizeParam(width, 0.01, 14),
+                  fontWeight: FontWeight.w600,
+                ),
+                const SizedBox(height: 15),
+                _TextItems(
+                  text: '4. ${StringRes.aboutFullNine}',
+                  fontSize: sizeParam(width, 0.01, 14),
+                  fontWeight: FontWeight.w600,
+                ),
+                const SizedBox(height: 30),
+                _TextItems(
+                  text: StringRes.aboutFullTen,
+                  fontSize: sizeParam(width, 0.015, 20),
+                  fontWeight: FontWeight.w800,
+                ),
+              ],
             ),
-            const BottomBar(),
-          ],
-        ),
+          ),
+          const BottomBar(),
+        ],
       ),
     );
   }
@@ -159,7 +173,9 @@ class _TextItems extends StatelessWidget {
 
   const _TextItems({
     Key? key,
-    required this.text, required this.fontSize, required this.fontWeight,
+    required this.text,
+    required this.fontSize,
+    required this.fontWeight,
   }) : super(key: key);
 
   @override
@@ -167,8 +183,7 @@ class _TextItems extends StatelessWidget {
     return Text(
       text,
       textAlign: TextAlign.justify,
-      style: TextStyle(
-          fontSize: fontSize, fontWeight: fontWeight),
+      style: TextStyle(fontSize: fontSize, fontWeight: fontWeight),
     );
   }
 }
@@ -189,31 +204,167 @@ class _MyText extends StatelessWidget {
     return Container(
         padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
         alignment: Alignment.center,
-        child: Expanded(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                textTitle,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              textTitle,
+              style: TextStyle(
+                  fontSize: sizeParam(width, 0.015, 20),
+                  fontFamily: 'Comfortaa',
+                  height: 1.2,
+                  fontWeight: FontWeight.w800),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            Text(textMain,
+                textAlign: TextAlign.justify,
                 style: TextStyle(
-                    fontSize: sizeParam(width, 0.015, 20),
+                    fontSize: sizeParam(width, 0.01, 14),
                     fontFamily: 'Comfortaa',
                     height: 1.2,
-                    fontWeight: FontWeight.w800),
-                textAlign: TextAlign.center,
-              ),
-              const SizedBox(
-                height: 30,
-              ),
-              Text(textMain,
-                  textAlign: TextAlign.justify,
-                  style: TextStyle(
-                      fontSize: sizeParam(width, 0.01, 14),
-                      fontFamily: 'Comfortaa',
-                      height: 1.2,
-                      fontWeight: FontWeight.w600)),
-            ],
-          ),
+                    fontWeight: FontWeight.w600)),
+          ],
         ));
+  }
+}
+
+class AboutSmall extends StatelessWidget {
+  const AboutSmall({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    var width = MediaQuery.of(context).size.width;
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          const SmallAppBar(
+            shadow: null,
+            colorDrawer: Colors.white,
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 20,right: 20,bottom: 20),
+            child: Column(
+              children: [
+                Align(
+                    alignment: Alignment.center,
+                    child: _TextItems(
+                      text: StringRes.aboutFullZero,
+                      fontSize: sizeParam(width, 0.015, 20),
+                      fontWeight: FontWeight.w800,
+                    )),
+                const SizedBox(height: 20,),
+                const _MyText(
+                  textMain: StringRes.aboutFullOne,
+                  textTitle: 'Наши приоритеты',
+                ),
+                FittedBox(
+                  child: SizedBox(
+                    height: width*0.7,
+                    child: Row(
+                      children: [
+                        Image.asset("assets/about/0.jpg"),
+                        Image.asset("assets/about/00.jpg"),
+                      ],
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 20,),
+                const _MyText(
+                  textMain: StringRes.aboutFullTwo,
+                  textTitle: 'Уникальность компании «ENKI»',
+                ),
+                FittedBox(
+                  child: SizedBox(
+                    height: width*0.7,
+                    child: Row(
+                      children: [
+                        Image.asset(
+                          'assets/about/1.jpg',
+                        ),
+                        Image.asset(
+                          'assets/about/2.jpg',
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 20,),
+                const _MyText(
+                  textMain: StringRes.aboutFullThree,
+                  textTitle: 'Индивидуальный подход к каждому',
+                ),
+                FittedBox(
+                  child: SizedBox(
+                    height: width*0.5,
+                    child: Image.asset(
+                      'assets/about/3.jpeg',
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 20,),
+                const _MyText(
+                  textMain: StringRes.aboutFullFour,
+                  textTitle: 'Пунктуальность',
+                ),
+                FittedBox(
+                  child: SizedBox(
+                    height: width*0.5,
+                    child: Image.asset(
+                      'assets/about/4.jpg',
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 20),
+                Align(
+                  alignment: Alignment.center,
+                  child: _TextItems(
+                    text: StringRes.aboutFullFive,
+                    fontSize: sizeParam(width, 0.015, 20),
+                    fontWeight: FontWeight.w800,
+                  ),
+                ),
+                const SizedBox(height: 30),
+                _TextItems(
+                  text: '1. ${StringRes.aboutFullSix}',
+                  fontSize: sizeParam(width, 0.01, 14),
+                  fontWeight: FontWeight.w600,
+                ),
+                const SizedBox(height: 15),
+                _TextItems(
+                  text: '2. ${StringRes.aboutFullSeven}',
+                  fontSize: sizeParam(width, 0.01, 14),
+                  fontWeight: FontWeight.w600,
+                ),
+                const SizedBox(height: 15),
+                _TextItems(
+                  text: '3. ${StringRes.aboutFullEight}',
+                  fontSize: sizeParam(width, 0.01, 14),
+                  fontWeight: FontWeight.w600,
+                ),
+                const SizedBox(height: 15),
+                _TextItems(
+                  text: '4. ${StringRes.aboutFullNine}',
+                  fontSize: sizeParam(width, 0.01, 14),
+                  fontWeight: FontWeight.w600,
+                ),
+                const SizedBox(height: 30),
+                _TextItems(
+                  text: StringRes.aboutFullTen,
+                  fontSize: sizeParam(width, 0.015, 20),
+                  fontWeight: FontWeight.w800,
+                ),
+              ],
+            ),
+          ),
+          const BottomBar(),
+        ],
+      ),
+    );
   }
 }
